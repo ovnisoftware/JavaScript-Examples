b5a368a374508a6917cea075f2622163183e3092
ovnisoftware <info@ovnisoftware.com>
2015-10-12 16:32:50 -0400

    Initial upload

---
+++ JavaScriptExamples/JavaScriptExamples/RockPaperScissors.aspx
@@ -0,0 +1,35 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="RockPaperScissors.aspx.cs" Inherits="JavaScriptExamples.RockPaperScissors" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <script src="js/RPS.js"></script>
+    <style>
+        #panel {
+            border: 5px solid black;
+            height: 200px;
+            width: 300px;
+            padding: 5px;
+            text-align: center;
+            background-color: yellow;
+            border-radius: 5px; /*rounds corners*/
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div id="panel">
+            Select Rock, Paper, or Scissors:<br />
+            <select id="Select1">
+                <option>Rock</option>
+                <option>Paper</option>
+                <option>Scissors</option>
+            </select>
+            <input id="Button1" type="button" value="Play!" onclick="check()" /><br />
+            <div id="panel2"></div>
+        </div>
+    </form>
+</body>
+</html>
---
+++ JavaScriptExamples/JavaScriptExamples/js/RPS.js
@@ -0,0 +1,50 @@
+﻿var countturns = 0;
+var wins = 0;
+var losses = 0;
+
+function check() {
+    debugger
+    var panel = document.getElementById("panel2");
+    var rannumber = Math.ceil(Math.random() * 3); //random number between 1 and 3
+    var userchoice = document.getElementById("Select1").value;
+    countturns++;
+
+    //Convert int rannumber into computerchoice text
+    switch (rannumber) {
+        case 1:
+            computerchoice = "Rock";
+            break;
+        case 2:
+            computerchoice = "Scissors";
+            break;
+        case 3:
+            computerchoice = "Paper";
+            break;
+    }
+
+    panel.innerHTML = "You threw " + userchoice + " and the computer threw " + computerchoice + "<br>";
+
+    //Determine winner by comparing computerChoice and userChoice
+    if (computerchoice == userchoice) {
+        panel.innerHTML += "Tie game!";
+    }
+    else if ((computerchoice == "Rock" && userchoice == "Scissors") ||
+        (computerchoice == "Paper" && userchoice == "Rock")) {
+        panel.innerHTML += "You lose!";
+        losses++;
+    }
+    else if ((computerchoice == "Scissors" && userchoice == "Rock") ||
+        (computerchoice == "Rock" && userchoice == "Paper")) {
+        panel.innerHTML += "You win!";
+        wins++;
+    }
+    //Win/loss conversion
+    var record = (wins / losses) * 100;
+    record = Math.ceil(record * Math.pow(10, 2)) / Math.pow(10, 2);
+
+    //Display results
+    panel.innerHTML += "<br>You've played this many games: " + countturns + "<br>";
+    panel.innerHTML += "You're win/loss ratio is: " + record + "%<br>";
+    panel.innerHTML += "Wins: " + wins + "<br>Losses: " + losses + "<br>";
+    panel.innerHTML += "<input id='Button2' type='submit' value='Restart stats'/>";
+}
